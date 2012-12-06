e56de9ecd24af8c4c8092a3639ee3cd90c138f0d
marelab <marchammermann@gmail.com>
2012-12-06 02:24:16 +0100

    changed svn to git

--- marelab-web/script/marelab/marelabdev_lan_EN.js
+++ marelab-web/script/marelab/marelabdev_lan_EN.js
@@ -3,7 +3,7 @@
 
 var mlang = new Array();
 mlang["marelabLinkTextWeb"] = "You find here all infos to marelab addons new boards plugins and the hole developer description";
-mlang["marelabLinkTextSVN"] = "Get the newest source code over our SVN google repository http://marelab-solar.googlecode.com/svn/";
+mlang["marelabLinkTextSVN"] = "Get the newest source code over our GIT google repository http://marelab-solar.googlecode.com/git/";
 mlang["marelabDevSystemTitle"] = "marelab development system";
 mlang["marelabLiveSystemtest"] = "Test the base marelab components";
 mlang["marelabLiveSystem"] = "start the marelab web gui";
