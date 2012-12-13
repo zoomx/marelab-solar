b4c09de1ce80a0adffdbe3777f95a5d0253e6a2b
terahz <terahz@gmail.com>
2012-12-12 20:06:01 -0500

    Adding Debug-BB-CrossCC configuration for BeagleBone builds using eclipse and ubuntu. Cleanup of old dirs

--- marelab-nucleus/Debug-ARM/src/json/jsmn.d
+++
@@ -1,3 +0,0 @@
-src/json/jsmn.d: ../src/json/jsmn.c ../src/json/jsmn.h
-
-../src/json/jsmn.h:
--- marelab-nucleus/Debug-ARM/src/marelab/LedString.d
+++
@@ -1,9 +0,0 @@
-src/marelab/LedString.d: ../src/marelab/LedString.cpp \
- ../src/marelab/LedString.h ../src/marelab/ipccom.h \
- ../src/marelab/marelabconf.h
-
-../src/marelab/LedString.h:
-
-../src/marelab/ipccom.h:
-
-../src/marelab/marelabconf.h:
